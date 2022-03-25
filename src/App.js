337bfdf8b45ef156432087680824fad36b17eeee
Kibria hossain <you@example.com>
2022-03-26 02:17:42 +0600

    order summary design complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { FaBeer } from 'react-icons/fa';
+
 import Header from './components/Header/Header';
 import { useEffect, useState } from 'react';
 import Cart from './components/Cart/Cart';
@@ -20,18 +20,25 @@ function App() {
   
 
   const addToCartBtn = (pen) => {
-    setCount(count + 1)
-    console.log(cart);
+    if (count >= 4) {
+      return;
+    }
     const newCart = [...cart, pen];
     setCart(newCart);
+    setCount(count + 1)
+    console.log(cart);
+
   }
+  const choseOnebtn = () => {
+      
+    }
 
   return (
     <div className='App'>
 
       {/*----- Navbar section --- */}
       <Header count ={count}></Header>
-      <p className='text-center header-content'>Select the best luxury writing instrument....</p>
+      <p className='text-center header-content'>Select <strong>4</strong> best luxury writing instrument....</p>
 
       {/* ------- Card section -------- */}
       <div className="cart-container d-grid">
@@ -41,10 +48,15 @@ function App() {
       }
     </div>
         <div className="summary-container">
-                <div className='order-container'>
+          <div className='order-container'>
+            <h4 className='ms-4 mt-4 mb-4'> Your Selected Pen</h4>
           {
             cart.map(selectedCart => <Summary key={selectedCart.id} selectedCart = {selectedCart}></Summary>)
-        }
+            }
+            <div className="button-div">
+               <button className='btn btn-success'>Chose One</button>
+               <button className='btn btn-danger ms-4'>Reset All</button>
+           </div>
       </div>
       </div>
       </div> {/*cart section end */}
--- src/Summary/Summary.js
+++ src/Summary/Summary.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Summary.css'
-
+import { MdDelete } from 'react-icons/md';
 
 const Summary = (props) => {
      const { name, img } = props.selectedCart;
 
 
-     return (
-          <div>
-               <div>
-               <h3>{ name}</h3>
+     return (  
+          <div className='d-flex ms-4 mt-1 selected-cart '>
+               <div className='me-4'><img src={img} style={{ width: '50px',border:'4px solid #c2fbd7', borderRadius:'50%'}} alt="" /></div>
+               <div className='d-flex align-items-center ' >
+                    <div>
+                         <h3 className='py-1'>{name}</h3>
+                    </div>
+                 <div> <  MdDelete className=' deleteIcon fs-3'/></div>
+                    
                </div>
-          </div>
+               </div>
      );
 };
 
