a49b052ad02b0f116d898f712d0feb71d89bfdf0
Kibria hossain <you@example.com>
2022-03-26 00:30:33 +0600

    cart design complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaBeer } from 'react-icons/fa';
+// import { FaBeer } from 'react-icons/fa';
 import Header from './components/Header/Header';
+import { useEffect, useState } from 'react';
+import Cart from './components/Cart/Cart';
 
 function App() {
-  
+  const [pens, setPens] = useState([]);
+  useEffect(() => {
+    fetch('product.json')
+      .then(res => res.json())
+    .then(data => setPens(data))
+  },[])
 
 
   return (
     <div className='App'>
+
+      {/*----- Navbar section --- */}
       <Header></Header>
-      <p className='text-center'>Select the best luxury writing instrument.</p>
+      <p className='text-center header-content'>Select the best luxury writing instrument....</p>
+
+      {/* ------- Card section -------- */}
+      <div className="cart-container d-grid">
+      <div className="cart-section">
+         {
+        pens.map(pen=><Cart key={pen.id} pen = {pen}></Cart>)
+      }
+    </div>
+      <div>
+        
+      </div>
+      </div> {/*cart section end */}
     </div>
   );
 }
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Cart.css';
+import { BsCartCheck } from 'react-icons/bs';
+
+
+const Cart = (props) => {
+     const { name, price, img, id, index, seller } = props.pen;
+     return (
+          <div className="cart">
+               <div>
+                    <img src= {img} alt="" />
+               </div>
+               <div className='ms-3 mt-3'>
+                    <h4>Pen's name: {name}</h4>
+                    <h5>Price:$ {price}</h5>
+                    <h5>Brand name: { seller}</h5>
+               </div>
+               <div>
+                    <button> Add to Cart <BsCartCheck style={{ marginLeft: '10px', marginBottom:'5px'}}/> </button>
+               </div>
+          </div>
+     );
+};
+
+export default Cart;
